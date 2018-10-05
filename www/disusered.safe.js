3be86b93b7faf26fe99c7d49af6e30d61ce17d19
Manuel Feller <ManuelFeller@users.noreply.github.com>
2018-10-05 15:32:17 +0200

    removed debug prints and made both wrapper using the callbacks directly
    on iOS the try to access the file from the javascript caused to exit the callback without any further notice. Since this version of the plugin can encrypt files in any of the application directories and my use case does not need the actual file (only the name is useful) I decided to remove the passing of the actual file and fall back to the path string

--- www/disusered.safe.js
+++ www/disusered.safe.js
@@ -2,7 +2,7 @@
  * disusered.safe.js
  *
  * @overview Easy to use cryptographic operations for Cordova.
- * @author Carlos Antonio
+ * @author Carlos Antonio; modified by Manuel Feller
  * @license MIT
 */
 
@@ -19,15 +19,7 @@ var safe = {
    * @returns {void}
    */
   encrypt: function(path, password, success, error) {
-    // var encryptSuccess, encryptError;
-    console.log('Cordova:entering encryption...');
     if (!path || arguments.length === 0) return;
-
-    // encryptSuccess = onSuccess.bind(null, success);
-    // encryptError = onError.bind(null, error);
-
-    // exec(encryptSuccess, encryptError, 'Safe', 'encrypt', [path, password]);
-    console.log('Cordova:executing encryption...');
     exec(success, error, 'Safe', 'encrypt', [path, password]);
   },
 
@@ -41,47 +33,10 @@ var safe = {
    * @returns {void}
    */
   decrypt: function(path, password, success, error) {
-    var decryptSuccess, decryptError;
-
     if (!path || arguments.length === 0) return;
-
-    decryptSuccess = onSuccess.bind(null, success);
-    decryptError = onError.bind(null, error);
-
-    exec(decryptSuccess, decryptError, 'Safe', 'decrypt', [path, password]);
+    exec(success, error, 'Safe', 'decrypt', [path, password]);
   }
 
 };
-
-/**
- * onSuccess
- *
- * @param {Function} success Success callback
- * @param {String} path Encrypted file URI
- * @returns {String} Encrypted file URI
- */
-function onSuccess(success, path) {
-  if (typeof success === 'function') {
-    window.requestFileSystem(window.PERSISTENT, 0, function(fs) {
-      fs.root.getFile(path.split('/').pop(), {create: false}, function(file) {
-        file.file(function(fileObj) {
-          success(fileObj);
-        }, onError);
-      }, onError);
-    }, onError);
-  }
-}
-
-/**
- * onError
- *
- * @param {String} error Error callback
- * @param {Function} code Error code
- * @returns {String} Decrypted file URI
- */
-function onError(error, code) {
-  if (typeof error === 'function') error(code);
-  return code;
-}
 
 exports.safe = safe;
